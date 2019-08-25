3af4de2768462a217ca14c7e52cc870d88be878d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-25 15:26:18 -0400

    Initilizes routes used for logging in/registering

---
+++ middleware/accounts.js
@@ -0,0 +1,53 @@
+const {
+  isValidEmail,
+  isValidUsername
+} = require('../config/inputEvaluation.js');
+const Users = require('../models/db/users');
+
+module.exports = { parseInput, verifyAccountInfo };
+
+function parseInput(req, res, next) {
+  res.locals.user = {};
+  res.locals.user.username = req.body.username
+    ? req.body.username.toLowerCase()
+    : null;
+  res.locals.user.email = req.body.email ? req.body.email.toLowerCase() : null;
+  res.locals.user.password = req.body.password;
+  res.locals.user.account = req.body.account
+    ? req.body.account.toLowerCase()
+    : null;
+  if (next) next();
+}
+
+async function verifyAccountInfo(req, res, next) {
+  parseInput(req, res);
+  const { username, email, password, account } = res.locals.user;
+
+  if (email && password) {
+    if (email && !isValidEmail(email)) {
+      return res.status(400).json({ message: 'Invalid email format' });
+    }
+
+    if (username && !isValidUsername(username)) {
+      return res.status(400).json({
+        message: 'Only alphanumeric characters allowed in a username'
+      });
+    }
+
+    const usernameExists = await Users.find({ username }).first();
+    const emailExists = await Users.find({ email }).first();
+
+    if (usernameExists) {
+      return res.status(400).json({ message: 'Username already in use' });
+    }
+    if (emailExists) {
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+  }
+  password && ((username && email) || account)
+    ? next()
+    : res.status(400).json({
+        message:
+          'If registering, provide password, username, and email.  If logging in, include account and password'
+      });
+}
---
+++ routes/auth/index.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const Users = require('../../models/db/users');
+const generateToken = require('../../config/generateToken.js');
+const { verifyAccountInfo } = require('../../middleware/accounts.js');
+const { isValidEmail } = require('../../config/inputEvaluation.js');
+
+router.post('/', verifyAccountInfo, async (req, res) => {
+  const { username, email, account, password } = res.locals.user;
+
+  /* REGISTER NEW ACCOUNT */
+  if (username && email) {
+    const newUser = {
+      username,
+      email,
+      password: bcrypt.hashSync(password, 10),
+      wins: 0,
+      losses: 0
+    };
+    const new_account = await Users.create(newUser);
+    delete new_account.password;
+    return res
+      .status(201)
+      .json({ ...new_account, token: generateToken(new_account) });
+
+    /* LOGIN TO EXISTING ACCOUNT */
+  } else if (account) {
+    const key = isValidEmail(account) ? 'u.email' : 'u.username';
+    const user = await Users.find({ [key]: account }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      return res.status(200).json({
+        ...user,
+        token: generateToken(user)
+      });
+    } else {
+      return res.status(401).json({ message: 'Invalid Credentials' });
+    }
+  }
+});
+
+module.exports = router;
