9bcd23c8f778b1dbb387e72f4def224bc3fd55b2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-08 03:33:23 -0400

    Fixes 'res.status' not a function bug

--- middleware/errorHandling.js
+++ middleware/errorHandling.js
@@ -1,4 +1,4 @@
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
   console.log('ERR: ', err);
   const statusCode = err.statusCode || 500;
   const env = process.env.DB_ENVIRONMENT || 'development';
