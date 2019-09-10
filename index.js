944369aa832252980f370516f1f072874becd443
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-09 23:11:25 -0400

    Adds socket.io

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ require('express-async-errors');
 require('app-module-path').addPath(__dirname);
 
 const server = require('./server.js');
+const http = require('http').createServer(server);
+const io = require('socket.io')(http, { path: '/' });
+
+io.on('connection', socket => {
+  console.log(socket);
+});
 
 const port = process.env.PORT || 4500;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
