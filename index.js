f05e2ff99344ea1b1bfb01a0c05ac1017d31130a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-21 13:57:36 -0400

    Turns on web sockets

--- index.js
+++ index.js
@@ -7,15 +7,8 @@ require('express-async-errors');
 // Allow absolute imports
 require('app-module-path').addPath(__dirname);
 
-const server = require('./server.js');
-const http = require('http').createServer(server);
-const io = require('socket.io')(http, { path: '/' });
-
-io.on('connection', socket => {
-  console.log(socket);
-});
+// Sockets
+const { http } = require('./sockets.js');
 
 const port = process.env.PORT || 4500;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
-
-server.on('listening', () => console.log('Listening on port'));
+http.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
