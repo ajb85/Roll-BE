763bd452ec87c663c8f27584b452e1ec951305a9
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-09-22 22:38:00 -0400

    Returns response for consistency

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   jwt.verify(token, secret, (err, decodedToken) => {
     if (err) {
-      res.status(401).json({ message: 'Invalid token' });
+      return res.status(401).json({ message: 'Invalid token' });
     } else {
       res.locals.token = decodedToken;
       next();
