0f4f9a651cdd48fc1e0bab6e95dad30b836b544b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-13 02:34:48 -0400

    Imports http from its new location

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('express-async-errors');
 require('app-module-path').addPath(__dirname);
 
 // Sockets
-const { http } = require('./sockets.js');
+const http = require('config/http.js');
 
 const port = process.env.PORT || 4500;
 http.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
