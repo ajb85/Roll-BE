13c1a0a3e994b96577784e99fdbdc2cb0114cb3a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-10-27 16:34:38 -0400

    Preparing to remove Knex

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.HEROKU_POSTGRESQL_IVORY_URL,
     pool: {
       min: 2,
       max: 10
--- models/index.js
+++ models/index.js
@@ -1,9 +1,40 @@
-// require('dotenv').config();
-
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+const { Pool, Client } = require('pg');
 
 const environment = process.env.NODE_ENV || 'development';
 console.log(`Running ${environment} build.`);
 
-module.exports = knex(knexConfig[environment]);
+let pool, client;
+
+if (environment === 'production') {
+  const connection = {
+    connectionString: process.env.HEROKU_POSTGRESQL_IVORY_URL
+  };
+  pool = new Pool(connection);
+  client = new Client(connection);
+} else if (environment === 'development') {
+  const connection = {
+    user: process.env.PG_USER,
+    host: 'localhost',
+    database: process.env.PG_DB,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PORT || 5432
+  };
+  pool = new Pool(connection);
+  client = new Client(connection);
+}
+
+pool.query('SELECT NOW()', err => {
+  if (err) {
+    console.log('PG Pool Connection Error: ', err);
+  }
+  pool.end();
+});
+
+client.connect();
+
+client.query('SELECT NOW()', err => {
+  if (err) {
+    console.log('PG Client Connection Error: ', err);
+  }
+  client.end();
+});
