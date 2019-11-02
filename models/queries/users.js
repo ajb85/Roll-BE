89d498c62e9a42786e5a2ade4929f3796eca42e3
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-02 18:13:11 -0400

    Adds new test queries

---
+++ models/queries/users.js
@@ -0,0 +1,31 @@
+const Query = require('../index.js');
+
+module.exports = { find, edit, create, remove };
+
+function find(filter, first) {
+  return new Query('users AS u')
+    .select('*')
+    .where(filter)
+    .first(first)
+    .run();
+}
+
+function edit(filter, newInfo) {
+  return new Query('users')
+    .update(newInfo)
+    .where(filter)
+    .first(true)
+    .then(u => find({ 'u.user': u.id }, true))
+    .run();
+}
+
+function create(newUser) {
+  return new Query('users')
+    .insert(newUser)
+    .then(u => find({ 'u.user': u.id }, true))
+    .run();
+}
+
+function remove(id) {
+  return new Query('users').delete({ id }).run();
+}
