867fb5a0c2b6a95740290bc595fe145a4f65af2b
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-02 18:10:28 -0400

    Updates to use node-postgres

--- models/index.js
+++ models/index.js
@@ -1,40 +1,123 @@
-const { Pool, Client } = require('pg');
+const { Client } = require('pg');
 
 const environment = process.env.NODE_ENV || 'development';
-console.log(`Running ${environment} build.`);
+const connection =
+  environment === 'production'
+    ? {
+        connectionString: process.env.HEROKU_POSTGRESQL_IVORY_URL
+      }
+    : environment === 'development'
+    ? {
+        user: process.env.PG_USER,
+        host: 'localhost',
+        database: process.env.PG_DB,
+        password: process.env.PG_PASSWORD,
+        port: process.env.PORT || 5432
+      }
+    : console.error(`Whoops, you didn't account for ${environment} builds`);
 
-let pool, client;
-
-if (environment === 'production') {
-  const connection = {
-    connectionString: process.env.HEROKU_POSTGRESQL_IVORY_URL
-  };
-  pool = new Pool(connection);
-  client = new Client(connection);
-} else if (environment === 'development') {
-  const connection = {
-    user: process.env.PG_USER,
-    host: 'localhost',
-    database: process.env.PG_DB,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PORT || 5432
-  };
-  pool = new Pool(connection);
-  client = new Client(connection);
-}
-
-pool.query('SELECT NOW()', err => {
-  if (err) {
-    console.log('PG Pool Connection Error: ', err);
-  }
-  pool.end();
-});
-
+const client = new Client(connection);
 client.connect();
 
-client.query('SELECT NOW()', err => {
-  if (err) {
-    console.log('PG Client Connection Error: ', err);
+module.exports = class Query {
+  constructor(table) {
+    this.client = client;
+    this.table = table;
+    this.values = [];
+
+    // Listed for ease of reading
+    this.first;
+    this.text;
+    this.then;
   }
-  client.end();
-});
+
+  run() {
+    console.log('QUERY: ', this.text);
+    console.log('VALUES: ', this.values);
+    return client
+      .query({ text: this.text, values: this.values })
+      .then(res => {
+        const data = this.first ? res.rows[0] : res.rows;
+        return this.then ? this.then(data) : data;
+      })
+      .catch(err => console.error('QUERY ERROR: ', err));
+  }
+
+  select(select) {
+    this.text = `SELECT ${select} FROM ${this.table}`;
+    return this;
+  }
+
+  update(newInfo, ...returnValues) {
+    if (!newInfo || !Object.keys(newInfo).length) {
+      return this;
+    }
+    this.text = 'UPDATE ' + this.table + this.text;
+    this.text += this._iterate('SET', newInfo, ',');
+    this.text += this._returning(returnValues);
+    return this;
+  }
+
+  insert(newUser, ...returnValues) {
+    if (!newUser || !Object.keys(newUser).length) {
+      return this;
+    }
+    this.text = 'INSERT INTO ' + this.table + this.text;
+    this.text += this._iterate('(', newUser, ',');
+    this.text += this._returning(returnValues);
+    return this;
+  }
+
+  delete(filter) {
+    this.text = 'DELETE FROM ' + this.table;
+    this.where(filter);
+
+    return this;
+  }
+
+  where(filter) {
+    if (!filter || !Object.keys(filter).length) {
+      return this;
+    }
+
+    const whereClause = this._iterate('WHERE', filter, ' AND');
+    this.text += whereClause;
+    return this;
+  }
+
+  first(boolean) {
+    this.first = boolean;
+    return this;
+  }
+
+  then(cb) {
+    this.then = cb;
+    return this;
+  }
+
+  _iterate(startTerm, data, joinTerm) {
+    const keys = Object.keys(data);
+    let str = ' ' + startTerm;
+
+    for (let i = 0; i < keys.length; i++) {
+      const index = this.values.length + 1;
+      const key = keys[i];
+
+      if (i > 0) {
+        str += joinTerm;
+      }
+
+      str += ` ${key} = $${index}`;
+      this.values.push(data[key]);
+    }
+    return str;
+  }
+
+  _returning(arg) {
+    if (!arg.length) {
+      arg = ['*'];
+    }
+
+    return ` RETURNING ${arg.join(', ')}`;
+  }
+};
