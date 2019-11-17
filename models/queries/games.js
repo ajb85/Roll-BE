efdf1de6eb2f3254ca10e526b79c29eafcb87172
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-17 16:56:40 -0500

    Removes dice join

--- models/queries/games.js
+++ models/queries/games.js
@@ -1,15 +1,6 @@
 const Query = require('../index.js');
 
-module.exports = {
-  find,
-  edit,
-  create,
-  join,
-  leave,
-  saveScore,
-  updateLastAction,
-  deactivate
-};
+module.exports = { find, edit, create, join, leave, saveScore };
 
 function find(filter, first) {
   return new Query('games AS g')
@@ -19,16 +10,15 @@ function find(filter, first) {
       'g.password AS password',
       'g.isActive AS isActive',
       'g.isJoinable AS isJoinable',
-      'array_agg(d.dice) AS dice',
+      // 'array_agg(DISTINCT ug.user_id) AS players'
       "array_agg(json_build_object('user_id', u.id, 'username', u.username)) as players",
       "array_agg(json_build_object('user_id', s.user_id, 'Ones', s.Ones, 'Twos', s.Twos, 'Threes', s.Threes, 'Fours', s.Fours, 'Fives', s.Fives, 'Sixes', s.Sixes, 'Left Total', s.Left Total, '3 of a Kind', s.3 of a Kind, '4 of a Kind', s.4 of a Kind, 'Full House', s.Full House, 'Sm Straight', s.Sm Straight, 'Lg Straight', s.Lg Straight, 'Roll!', s.Roll!, 'Roll Bonus', s.Roll! Bonus, 'Free Space', s.Free Space, 'Grand Total', s.Grand Total)) as scores"
     )
     .join('users_in_game AS ug', { 'ug.game_id': 'g.id' }, 'LEFT')
     .join('scores AS s', { 's.game_id': 'g.id' }, 'LEFT')
     .join('users AS u', { 'u.id': 'ug.user_id' }, 'FULL OUTER')
-    .join('dice AS d', { 'd.user_id': 'ug.user_id' })
-    .where({ ...filter, 'g.isActive': true })
-    .groupBy('g.id', 'd.game_id')
+    .where(filter)
+    .groupBy('g.id', 'ug.game_id')
     .first(first)
     .run();
 }
@@ -91,23 +81,6 @@ function saveScore(filter, newScore) {
     .then(async s => {
       await Dice.clearRolls(filter);
       return find({ 'g.id': s.game_id }, true);
-    })
-    .run();
-}
-
-function updateLastAction(id) {
-  return new Query('games').update({ last_action: new Date() }).where({ id });
-}
-
-function deactivate(id) {
-  return new Query('scores')
-    .delete({ game_id: id })
-    .then(async _ => {
-      await new Query('users_in_game').delete({ game_id: id }).run();
-      return new Query('games')
-        .update({ isActive: false, isJoinable: false })
-        .where({ id })
-        .run();
     })
     .run();
 }
