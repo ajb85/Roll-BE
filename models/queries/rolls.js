c4e70aa750969809a7023e75411b82ce1cdea5d0
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-11-24 18:50:11 -0500

    Renames table

--- models/queries/rolls.js
+++ models/queries/rolls.js
@@ -3,7 +3,7 @@ const Query = require('../index.js');
 module.exports = { find, saveRoll, clearRolls };
 
 function find(filter, first) {
-  return new Query('dice')
+  return new Query('rolls')
     .select('*')
     .where(filter)
     .first(first)
@@ -11,15 +11,13 @@ function find(filter, first) {
 }
 
 function saveRoll(newRoll) {
-  return (
-    new Query('dice')
-      .insert(newRoll)
-      .first(true)
-      // .then(d => find({ id: d.id }))
-      .run()
-  );
+  return new Query('rolls')
+    .insert(newRoll)
+    .first(true)
+    .then(d => find({ id: d.id }, true))
+    .run();
 }
 
 function clearRolls(filter) {
-  return new Query('dice').delete(filter).run();
+  return new Query('rolls').delete(filter).run();
 }
