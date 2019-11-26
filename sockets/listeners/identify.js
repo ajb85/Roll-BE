16fe0e01e0a6432e491084b0e0b2790c6d014756
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-11-26 03:01:17 -0500

    Removes Socket class to retain original socket class

--- sockets/listeners/identify.js
+++ sockets/listeners/identify.js
@@ -2,24 +2,31 @@ const jwt = require('jsonwebtoken');
 const secret = require('config/secret.js');
 const Users = require('models/queries/users.js');
 
-class Socket {
-  constructor(socket, user_id) {
-    for (let key in socket) {
-      this[key] = socket[key];
-    }
-    this.user = Users.find({ 'u.id': user_id }, true).then(u => u);
-  }
-}
+// class Socket {
+//   constructor(socket) {
+//     for (let key in socket) {
+//       this[key] = socket[key];
+//     }
+//     this.user;
+//     Object.setPrototypeOf(this, socket.prototype);
+//   }
+// }
 
 module.exports = function(socket, token) {
   if (!this.connected[socket.id] && token) {
     jwt.verify(token, secret, (err, decodedToken) => {
       if (!err) {
         const { user_id } = decodedToken;
-        this.connected[socket.id] = new Socket(socket, user_id);
-        this.connected[socket.id].user.then(u => {
+        this.connected[socket.id] = socket;
+        Users.find({ 'u.id': user_id }, true).then(u => {
           if (u) {
-            console.log('IDENTIFIED SOCKET: ', u.username);
+            this.connected[socket.id].user = u;
+            console.log(
+              'IDENTIFIED SOCKET: ',
+              this.connected[socket.id].user.username,
+              'CONNECTED: ',
+              Object.keys(this.connected).length
+            );
           }
         });
         this.userToSocket[user_id] = socket.id;
