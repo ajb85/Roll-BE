6d3cdaf81d2b5b074c7c8d4b9de5c9bbcd16bcc2
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-12-23 04:41:57 -0500

    Comments out security hole

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -43,23 +43,23 @@ router.post('/', verifyAccountInfo, async (req, res) => {
   }
 });
 
-router.put('/update/:id', async (req, res) => {
-  // const { user_id: id } = res.locals.token;
-  const { id } = req.params;
-  if (req.body.password) {
-    const password = bcrypt.hashSync(req.body.password, 10);
+// router.put('/update/:id', async (req, res) => {
+//   // const { user_id: id } = res.locals.token;
+//   const { id } = req.params;
+//   if (req.body.password) {
+//     const password = bcrypt.hashSync(req.body.password, 10);
 
-    const updated = await Users.edit({ id }, { password });
+//     const updated = await Users.edit({ id }, { password });
 
-    return updated
-      ? res.status(200).json({ message: 'Account updated!' })
-      : res
-          .status(400)
-          .json({ message: 'Something went wrong updating the account' });
-  } else {
-    return res.status(400).json({ message: 'No password' });
-  }
-});
+//     return updated
+//       ? res.status(200).json({ message: 'Account updated!' })
+//       : res
+//           .status(400)
+//           .json({ message: 'Something went wrong updating the account' });
+//   } else {
+//     return res.status(400).json({ message: 'No password' });
+//   }
+// });
 
 router.get('/', auth, (req, res) => {
   const { user } = res.locals;
