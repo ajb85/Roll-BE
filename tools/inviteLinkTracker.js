4f0f063cc8f9cd4896a4668f1635caa01d54f2b2
ajb85 <19802883+ajb85@users.noreply.github.com>
2019-12-24 19:28:39 -0500

    Tracks uuids in use for join links

---
+++ tools/inviteLinkTracker.js
@@ -0,0 +1,43 @@
+const uuid = require('uuid/v1');
+
+class Tracker {
+  constructor() {
+    this.games = {};
+    this.gameIDToUUID = {};
+  }
+
+  add(game_id) {
+    if (this.gameIDToUUID[game_id]) {
+      // If the game already exists here, delete the old entry
+      const game_uuid = this.gameIDToUUID[game_id];
+      delete this.gameIDToUUID[game_id];
+      delete this.games[game_uuid];
+    }
+
+    let newID;
+    while (!newID || this.games[newID]) {
+      // Yes, I'm aware how unlikely this is to ever run more than once
+      newID = uuid();
+    }
+
+    this.games[newID] = game_id;
+    this.gameIDToUUID[game_id] = newID;
+
+    return newID;
+  }
+
+  find(uuid) {
+    return this.games[uuid];
+  }
+
+  delete(game_id) {
+    const game_uuid = this.gameIDToUUID[game_id];
+    const didDelete = !!this.games[game_uuid];
+    delete this.games[game_uuid];
+    delete this.gameIDToUUID[game_id];
+
+    return didDelete;
+  }
+}
+
+module.exports = new Tracker();
