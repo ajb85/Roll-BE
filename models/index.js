dd2371888a6ffaaf04eea20aa5875f7708b28a34
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-06 03:19:22 -0500

    Linter fixes

--- models/index.js
+++ models/index.js
@@ -1,18 +1,18 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || "development";
 const connection =
-  environment === 'production'
+  environment === "production"
     ? {
-        connectionString: process.env.DATABASE_URL
+        connectionString: process.env.DATABASE_URL,
       }
-    : environment === 'development'
+    : environment === "development"
     ? {
         user: process.env.PG_USER,
-        host: 'localhost',
+        host: "localhost",
         database: process.env.PG_DB,
         password: process.env.PG_PASSWORD,
-        port: process.env.PORT || 5432
+        port: process.env.PORT || 5432,
       }
     : console.error(`Whoops, you didn't account for ${environment} builds`);
 
@@ -24,7 +24,7 @@ module.exports = class Query {
     this.client = client;
     this.table = table;
     this.values = [];
-    this.text = '';
+    this.text = "";
 
     // Listed for ease of reading
     this.first;
@@ -33,22 +33,22 @@ module.exports = class Query {
   }
 
   run() {
-    // console.log('QUERY: ', this.text);
-    // console.log('VALUES: ', this.values);
+    // console.log("QUERY: ", this.text);
+    // console.log("VALUES: ", this.values);
     if (this.returning) {
       this.text += ` RETURNING ${this.returning}`;
     }
 
     return this.client
       .query({ text: this.text, values: this.values })
-      .then(res => {
+      .then((res) => {
         const data = this.first ? res.rows[0] : res.rows;
         if (!this.callback) {
           // console.log('RETURNING DATA: ', data);
         }
         return this.callback ? this.callback(data) : data;
       })
-      .catch(err => console.error('QUERY ERROR: ', err));
+      .catch((err) => console.error("QUERY ERROR: ", err));
   }
 
   table(table) {
@@ -57,25 +57,25 @@ module.exports = class Query {
 
   select(...select) {
     const formattedStr = select
-      .map(x => {
-        if (x.substring(0, 4).toLowerCase() === 'case') {
+      .map((x) => {
+        if (x.substring(0, 4).toLowerCase() === "case") {
           return x;
         }
         const toSplit =
-          x.indexOf('AS') > 0 ? ' AS ' : x.indexOf('as') ? ' as ' : ' ';
+          x.indexOf("AS") > 0 ? " AS " : x.indexOf("as") ? " as " : " ";
         const split = x.split(toSplit);
-        if (split[0] === '*') {
+        if (split[0] === "*") {
           return x;
         }
         if (split.length <= 1) {
           return this._dotQuotes(x);
         }
-        return split.map(y => this._dotQuotes(y)).join(toSplit);
+        return split.map((y) => this._dotQuotes(y)).join(toSplit);
       })
-      .join(', ');
+      .join(", ");
 
     if (this.text.length) {
-      this.text += ' ';
+      this.text += " ";
     }
 
     this.text += `SELECT ${formattedStr} FROM ${this.table}`;
@@ -87,15 +87,15 @@ module.exports = class Query {
       return this;
     }
 
-    this.text = 'UPDATE ' + this.table + this.text;
-    this.text += this._iterateEquals('SET', newInfo, ',');
+    this.text = "UPDATE " + this.table + this.text;
+    this.text += this._iterateEquals("SET", newInfo, ",");
     this._returning(returnValues);
     return this;
   }
 
   insert(newUser, ...returnValues) {
     if (newUser && Object.keys(newUser).length) {
-      this.text = 'INSERT INTO ' + this.table;
+      this.text = "INSERT INTO " + this.table;
       this.text += this._iterateColumnsAndValues(newUser);
       this._returning(returnValues);
     }
@@ -104,7 +104,7 @@ module.exports = class Query {
   }
 
   delete(filter) {
-    this.text = 'DELETE FROM ' + this.table;
+    this.text = "DELETE FROM " + this.table;
     this.where(filter);
 
     return this;
@@ -115,7 +115,7 @@ module.exports = class Query {
       return this;
     }
 
-    const whereClause = this._iterateEquals('WHERE', filter, ' AND');
+    const whereClause = this._iterateEquals("WHERE", filter, " AND");
     this.text += whereClause;
     return this;
   }
@@ -130,28 +130,32 @@ module.exports = class Query {
     return this;
   }
 
-  join(table, conditions, joinType = 'INNER') {
+  join(table, conditions, joinType = "INNER") {
     this.text += ` ${joinType.toUpperCase()} JOIN ${table}${this._iterateEqualsLiteral(
-      'ON',
+      "ON",
       conditions,
-      ' AND'
+      " AND"
     )}`;
     return this;
   }
 
   groupBy(...arg) {
-    this.text += ` GROUP BY ${arg.map(str => this._dotQuotes(str)).join(', ')}`;
+    this.text += ` GROUP BY ${arg
+      .map((str) => this._dotQuotes(str))
+      .join(", ")}`;
     return this;
   }
 
   orderBy(...arg) {
-    this.text += ` ORDER BY ${arg.map(str => this._dotQuotes(str)).join(', ')}`;
+    this.text += ` ORDER BY ${arg
+      .map((str) => this._dotQuotes(str))
+      .join(", ")}`;
     return this;
   }
 
   raw(text) {
     if (this.text.length) {
-      this.text += ' ';
+      this.text += " ";
     }
     this.text += text;
 
@@ -161,16 +165,16 @@ module.exports = class Query {
   _dotQuotes(str) {
     const exitString = {
       json_build_object: true,
-      row_to_json: true
+      row_to_json: true,
     };
-    const openParensIndex = str.indexOf('(');
+    const openParensIndex = str.indexOf("(");
     if (openParensIndex > -1) {
-      const closeParensIndex = str.lastIndexOf(')');
+      const closeParensIndex = str.lastIndexOf(")");
       const aggFunctionName = str.substring(0, openParensIndex).toLowerCase();
 
       if (exitString[aggFunctionName]) {
         return `${str.substring(0, openParensIndex + 1)}${this._buildObject(
-          str.substring(openParensIndex + 1, closeParensIndex).split(', ')
+          str.substring(openParensIndex + 1, closeParensIndex).split(", ")
         )})`;
       }
 
@@ -179,22 +183,22 @@ module.exports = class Query {
       )}${str.substring(closeParensIndex)}`;
     }
 
-    const commaIndex = str.indexOf(',');
+    const commaIndex = str.indexOf(",");
     if (commaIndex > -1) {
       return str
-        .split(', ')
-        .map(x => this._dotQuotes(x))
-        .join(', ');
+        .split(", ")
+        .map((x) => this._dotQuotes(x))
+        .join(", ");
     }
 
-    const dotIndex = str.indexOf('.');
+    const dotIndex = str.indexOf(".");
     if (dotIndex > 0) {
       const afterDotStr = str.substring(dotIndex + 1);
-      const parensIndex = str.indexOf(')');
+      const parensIndex = str.indexOf(")");
 
       const quotationsStr =
         parensIndex === -1
-          ? afterDotStr === '*'
+          ? afterDotStr === "*"
             ? afterDotStr
             : `"${afterDotStr}"`
           : `"${str.substring(dotIndex + 1, parensIndex)}"${y.substring(
@@ -204,7 +208,7 @@ module.exports = class Query {
     } else {
       return str[0] === '"' || str[0] === "'"
         ? str
-        : str[0] === '/'
+        : str[0] === "/"
         ? str.substring(1, str.length - 1)
         : `"${str}"`;
     }
@@ -212,7 +216,7 @@ module.exports = class Query {
 
   _iterateEquals(startTerm, data, joinTerm) {
     const keys = Object.keys(data);
-    let str = startTerm ? ' ' + startTerm : '';
+    let str = startTerm ? " " + startTerm : "";
 
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
@@ -230,17 +234,17 @@ module.exports = class Query {
 
   _buildObject(arr) {
     if (arr.length <= 1) {
-      return arr.join('');
+      return arr.join("");
     }
 
-    let str = '';
+    let str = "";
     for (let i = 0; i < arr.length; i += 2) {
       // Will ignore mismatched number of key/value pairs
       const key = arr[i];
       const value = arr[i + 1];
 
       if (i > 0) {
-        str += ', ';
+        str += ", ";
       }
 
       // this.values.push(key);
@@ -251,7 +255,7 @@ module.exports = class Query {
 
   _iterateEqualsLiteral(startTerm, data, joinTerm) {
     const keys = Object.keys(data);
-    let str = startTerm ? ' ' + startTerm : '';
+    let str = startTerm ? " " + startTerm : "";
 
     for (let i = 0; i < keys.length; i++) {
       const key = keys[i];
@@ -276,13 +280,13 @@ module.exports = class Query {
       values.push(`$${index}`);
     }
 
-    return '(' + keys.join(', ') + ') VALUES(' + values.join(', ') + ')';
+    return "(" + keys.join(", ") + ") VALUES(" + values.join(", ") + ")";
   }
 
   _returning(arg) {
     if (!arg.length) {
-      arg = ['*'];
+      arg = ["*"];
     }
-    this.returning = arg.join(', ');
+    this.returning = arg.join(", ");
   }
 };
