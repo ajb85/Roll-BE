5dbb8e0e8e4b7c695ec85f1d603899c424bb0318
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-15 18:57:49 -0400

    updates prod env to use same as local

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,11 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: process.env.DATABASE_URL,
+    connection: {
+    host : process.env.PG_DB,
+    user : process.env.PG_USER,
+    password : process.env.PG_PASSWORD
+  },
     pool: {
       min: 2,
       max: 10,
