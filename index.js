8c6b54ab245ae3c2cadc66c003edd7356ece2069
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-03-20 15:06:15 -0400

    Adds CORS headers to response

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-
-const serverless = require('serverless-http');
+const serverless = require("serverless-http");
 // use environment variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Automated error handling
-require('express-async-errors');
+require("express-async-errors");
 
 // Allow absolute imports
-require('app-module-path').addPath(__dirname);
+require("app-module-path").addPath(__dirname);
 
-require('config/api.js');
-const server = require('config/server.js');
+require("config/api.js");
+const server = require("config/server.js");
 
 // const port = process.env.PORT || 4500;
 // http.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
 
-exports.handler = serverless(server);
+exports.handler = serverless(server, {
+  response(response, event, context) {
+    // the return value is always ignored
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type";
+    response.headers["Access-Control-Allow-Origin"] = "https://play-roll.com/";
+    response.headers["Access-Control-Allow-Methods"] =
+      "OPTIONS,POST,GET,PUT,DELETE";
+  },
+});
