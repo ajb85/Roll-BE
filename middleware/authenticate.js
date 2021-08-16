87e4c4825b5065cd404c50f81c0873e0c8c39228
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-08-15 21:44:22 -0400

    reduces to a single 'invalid token' response

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,25 +1,22 @@
-const jwt = require('jsonwebtoken');
-const secret = require('config/secret.js');
-const Users = require('models/queries/users.js');
+const jwt = require("jsonwebtoken");
+const secret = require("config/secret.js");
+const Users = require("models/queries/users.js");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-  jwt.verify(token, secret, (err, decodedToken) => {
-    if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    } else {
+  jwt.verify(token, secret, async (err, decodedToken) => {
+    try {
+      if (err) throw "invalid user";
+
       res.locals.token = decodedToken;
-      Users.find({ id: decodedToken.user_id }, true).then(u => {
-        if (u) {
-          res.locals.user = u;
-          next();
-        } else {
-          console.error('AUTH ERROR: user not found by ID');
-          return res
-            .status(401)
-            .json({ message: "That account doesn't exist!" });
-        }
-      });
+      const u = await Users.find({ id: decodedToken.user_id }, true);
+
+      if (!u) throw "invalid account";
+
+      res.locals.user = u;
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid token" });
     }
   });
 };
