7247d099c7ed4766e4b71b43ed180484805344a1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-08-15 21:15:48 -0400

    chore: reorganizes server vs api

--- config/api.js
+++ config/api.js
@@ -1,4 +1,11 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const server = require("./server");
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
 // Middleware
 const errorHandler = require("middleware/errorHandling.js");
