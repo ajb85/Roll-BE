006536dde4bc00988ac144824b7167371931f122
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 22:38:45 -0400

    disables unrecognized ssl

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: { connectionString: process.env.DATABASE_URL, ssl: true },
+    connection: { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } },
     pool: {
       min: 2,
       max: 10,
