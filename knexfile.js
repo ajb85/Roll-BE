592da4f380362b8f158b4b19d0181893f2803d89
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-12 00:50:50 -0400

    switches ssl to true

--- knexfile.js
+++ knexfile.js
@@ -22,12 +22,8 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: {
-      connectionString: process.env.DATABASE_URL + "?sslmode=require",
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    connection: process.env.DATABASE_URL,
+    ssl: true,
     pool: {
       min: 2,
       max: 10,
