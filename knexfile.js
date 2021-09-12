5a4300174249d3199de4926782eab46118e59eb3
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-12 00:07:13 -0400

    disables SSL

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } },
+    connection: { connectionString: process.env.DATABASE_URL, ssl: false },
     pool: {
       min: 2,
       max: 10,
