9ff9154ab6a7296f4918d11cdc562246c79978a7
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-12 00:16:39 -0400

    reverts back to ssl with no reject unauth

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,12 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: { connectionString: process.env.DATABASE_URL, ssl: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 10,
