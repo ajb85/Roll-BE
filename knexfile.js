f6b2684a3ddff066d7c5400e9b36286fefb7cd0c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-12 01:07:19 -0400

    collapses conection obj

--- knexfile.js
+++ knexfile.js
@@ -22,9 +22,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-    },
+    connection: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
