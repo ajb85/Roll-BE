3113653d1eea2d024b6b24837e1fee4b5a021d44
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-12 02:14:33 -0400

    removes logs

--- middleware/accounts.js
+++ middleware/accounts.js
@@ -4,12 +4,10 @@ const Users = require("../models/queries/users.js");
 module.exports = { verifyAccountInfo };
 
 async function verifyAccountInfo(req, res, next) {
-  console.log("VERIFYING ACCOUNT INFO");
   res.locals.user = req.body;
   const { username, email, password, account } = req.body;
 
   if (email && password) {
-    console.log("EMAIL & PASSWORD");
     if (email && !isValidEmail(email)) {
       return res.status(400).json({ requestType: "register", message: "Invalid email format" });
     }
@@ -20,10 +18,10 @@ async function verifyAccountInfo(req, res, next) {
         message: "Only alphanumeric characters allowed in a username",
       });
     }
-    console.log("QUERY DB");
+
     const usernameExists = await Users.find({ username }, true);
     const emailExists = await Users.find({ email }, true);
-    console.log("QUERY DONE");
+
     if (usernameExists) {
       return res.status(400).json({ requestType: "register", message: "Username already in use" });
     }
@@ -32,7 +30,7 @@ async function verifyAccountInfo(req, res, next) {
       return res.status(400).json({ requestType: "register", message: "Email already in use" });
     }
   }
-  console.log("VERIFIED ACCOUNT", !!(password && ((username && email) || account)));
+
   return password && ((username && email) || account)
     ? next()
     : res.status(400).json({
