857dd0eaf4fb721c773e06f5409205f22aadacff
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 23:26:16 -0400

    removes logs

--- config/api.js
+++ config/api.js
@@ -2,13 +2,10 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const server = require("./server");
-server.use((req, res, next) => {
-  console.log("INC REQ: ", req);
-  next();
-});
-server.use(cors());
+
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
 // Middleware
 const errorHandler = require("middleware/errorHandling.js");
