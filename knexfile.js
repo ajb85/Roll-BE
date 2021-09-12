b8fa8cec0ca3755349d2e28e85a362b5ac07fc1f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 22:34:57 -0400

    enables ssl

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: process.env.DATABASE_URL,
+    connection: { connectionString: process.env.DATABASE_URL, ssl: true },
     pool: {
       min: 2,
       max: 10,
