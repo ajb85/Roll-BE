b50c20f2386fb24c729123a14f5548b3b3bd102e
AJ Brush <19802883+ajb85@users.noreply.github.com>
2021-09-11 22:31:19 -0400

    drops connection object for url

--- knexfile.js
+++ knexfile.js
@@ -22,11 +22,7 @@ module.exports = {
   },
   production: {
     client: "postgresql",
-    connection: {
-    host : process.env.PG_DB,
-    user : process.env.PG_USER,
-    password : process.env.PG_PASSWORD
-  },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
